7a697b84d29d6476b78d00b8835120eb8f32daae
akrck02 <aketza.vazquez@gmail.com>
2021-09-27 11:14:00 +0200

    [UD 2] Ex 2

--- php/UD2/Ejercicio3.php
+++ php/UD2/Ejercicio3.php
@@ -2,44 +2,116 @@
 require_once "./styles/components.php";
 require_once "./lib/data.php";
 
-define("UPLOAD_DIR","./data/");
+define("UPLOAD_DIR", "./data/");
+define("DB", UPLOAD_DIR . "articulos.txt");
 
-function get_articles($url)
+/**
+ * Get articles from the file
+ */
+function get_items($url)
 {
-    $articles = [];
+    $items = [];
     $html = file_get_contents($url);
 
     $temp = explode("\n", $html);
     foreach ($temp as $line) {
-        $article = explode(";", $line);
-        if (count($article) >= 2)
-            $articles[] = $article;
+        $item = explode(";", $line);
+        if (count($item) >= 2)
+            $items[$item[0]] = $item;
     }
 
-    return $articles;
+    return $items;
 }
+
+/**
+ * Get the line of an article or  
+ */
+function get_item_line($url, $item)
+{
+
+    $file = fopen($url, "r");
+    $line = "";
+    $i = 0;
+    while (!feof($file)) {
+        $line = fgets($file);
+        $i++;
+        if (strpos($line, $item) !== false) {
+            fclose($file);
+            return $i;
+            break;
+        }
+    }
+    fclose($file);
+    return false;
+}
+
+/**
+ * Replaces an item on the file
+ */
+function replace_item($url, $item, $line)
+{
+    $html = file_get_contents($url);
+    $temp = explode("\n", $html);
+
+    if (count($item) >= 3)
+        $temp[$line - 1] = $item[0] . ";" . $item[1] . ";" . $item[2];
+    else if (count($item) == 2)
+        $temp[$line - 1] = $item[0] . ";" . $item[1];
+
+    $html = implode("\n", $temp);
+    file_put_contents($url, $html);
+}
+
+
+/**
+ * Adds a file if it isn't on the file
+ * or replace it if so,
+ */
+function add_or_replace_article($url, $item)
+{
+    if(count($item) == 0)
+        return false;
+
+    $article_line = get_item_line($url, $item[0]);
+
+    if ($article_line !== false)
+        replace_item($url, $item, $article_line);
+    else
+        addLineToFile($url, $item);
+}
+
+
+/**
+ *  Checkingg parameters
+ */
 
 $total = isset($_GET["total"]) ? $_GET["total"] : 0;
 $product_price = isset($_POST["product_price"]) ? $_POST["product_price"] : null;
 $product_name = isset($_POST["product_name"]) ? $_POST["product_name"] : null;
 $product_file = isset($_FILES["product_file"]) ? $_FILES["product_file"] : null;
 
-$articles = get_articles("./data/articulos.txt");
+
 if ($product_name != null and $product_price !== null) {
-  
-    if($product_file !== null) {
-        if (move_uploaded_file($_FILES['product_file']['tmp_name'],  UPLOAD_DIR . basename($product_name . ".txt") )) {
-            addLineToFile("./data/articulos.txt", $product_name . ";" . UPLOAD_DIR . basename($product_name . ".txt"));
-            $articles[] = [$product_name, $product_price, UPLOAD_DIR . basename($product_name . ".txt")];
+
+    $article_line = get_item_line(DB, $product_name);
+
+    echo "DEBUG MODE: <hr>";
+    echo "OBJECTO ENCONTRADO EN LINEA " . $article_line . "<br>";
+    print_r($product_file);
+
+    if ($product_file !== null) {
+        if(isset($product_file['size']) and $product_file['size'] > 0){
+            if (move_uploaded_file($_FILES['product_file']['tmp_name'], UPLOAD_DIR.$product_name.".txt")) 
+                add_or_replace_article($temp_url, [$product_name, $product_price, DB]);
+            else 
+                echo "<p style='color:crimson'>ERROR: El archivo no se puede subir, intentélo de nuevo.</p>";
         } else {
-            $articles[] = [$product_name, $product_price];
-            echo "<p style='color:crimson'>ERROR: El archivo no se puedo subir, intentélo de nuevo.</p>";
-        } 
-           
-    } else {
-        addLineToFile("./data/articulos.txt", $product_name . ";" . $product_price);
-    }
+            add_or_replace_article(DB, [$product_name,$product_price]);
+        }
+        
+    } else add_or_replace_article(DB, [$product_name,$product_price]);
 }
+$articles = get_items(DB);
 
 ?>
 
@@ -52,9 +124,9 @@ if ($product_name != null and $product_price !== null) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        <?php 
-            include_once "./styles/styles.css"; 
-            include_once "./styles/Ej3.css"; 
+        <?php
+        include_once "./styles/styles.css";
+        include_once "./styles/Ej3.css";
         ?>
     </style>
 </head>
@@ -64,7 +136,7 @@ if ($product_name != null and $product_price !== null) {
     <table>
         <?php
         foreach ($articles as $article) {
-            $article[2] = isset($article[2]) ? "<a href='".$article[2]."'>Archivo</a>" : "";
+            $article[2] = isset($article[2]) ? "<a href='" . $article[2] . "'>Archivo</a>" : "";
             echo "<tr>
                     <td>$article[0]</td>
                     <td>$article[1]€</td>
@@ -78,7 +150,7 @@ if ($product_name != null and $product_price !== null) {
 
     <h2>AÑADE ARTICULO</h2>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
-    
+
         <table>
             <tr>
                 <th>Nombre:</th>
