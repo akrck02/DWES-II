8d2e39e7ecbb5c1d1d7a7e8c5784e17df1da6671
akrck02 <aketza.vazquez@gmail.com>
2021-11-23 14:15:50 +0100

    [Java][T1][Ex1] Done.

---
+++ java/Servlets/Tanda1/Ejercicio1/src/main/java/ejercicio1/beans/ConversionCF.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1.beans;
+
+/**
+ *
+ * @author aketz
+ */
+public class ConversionCF {
+    
+    private double celsius;
+    private double fahrenheit; 
+    
+    /**
+     * Constructor of the converter
+     * gets value and temperature type ('f' or 'c')
+     * if type does not match celsius is selected
+     * @param temp temperature value
+     * @param type temperature type
+     */
+    public ConversionCF(double temp, String type){
+        
+        switch (type) {
+            case "f":
+                this.fahrenheit = temp;
+                this.celsius = fahrenheitToCelsius(temp);
+                break;
+            
+            //if type does not match celsius is selected by default
+            case "c":
+            default:
+                this.celsius = temp;
+                this.fahrenheit = celsiusToFahrenheit(temp);
+                break;
+        }
+        
+    }
+
+    /**
+     * Get celsius temperature
+     * @return celsius
+     */
+    public double getCelsius() {
+        return celsius;
+    }
+
+    /**
+     * Get fahrenheit temperature
+     * @return fahrenheit
+     */
+    public double getFahrenheit() {
+        return fahrenheit;
+    }
+    
+    /**
+     * Convert from celsius to fahrenheit
+     * @param celsius 
+     * @return fahrenheit temperature
+     */
+    private static double celsiusToFahrenheit( double celsius ){
+        return (celsius * 9 / 5) + 32 ;
+    }
+    
+    /**
+     * Convert from fahrenheit to celsius
+     * @param fahrenheit
+     * @return celsius temperature
+     */
+    private static double fahrenheitToCelsius( double fahrenheit ){
+        return (fahrenheit - 32) * 5 / 9;
+    }
+    
+}
---
+++ java/Servlets/Tanda1/Ejercicio1/src/main/java/ejercicio1/servlets/conversor.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package ejercicio1.servlets;
+
+import ejercicio1.beans.ConversionCF;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aketz
+ */
+public class conversor extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        if (request.getParameter("cel-fah") != null) {
+            final String celsius = request.getParameter("celsius");
+
+            if (celsius == null || celsius.equals("")) {
+                errorPage(request, response, "No ha indicado los grados celsius");
+                return;
+            }
+
+            try {
+                
+                final double value = Double.parseDouble(celsius);
+                final ConversionCF conversion = new ConversionCF(value, "c");
+                resultPage(request, response, conversion.getCelsius(), conversion.getFahrenheit());
+            
+            } catch (NumberFormatException e) {
+                errorPage(request, response, "La temperatura debe ser númerica");
+            }
+
+        } else if (request.getParameter("fah-cel") != null) {
+            final String fahrenheit = request.getParameter("fahrenheit");
+
+            if (fahrenheit == null || fahrenheit.equals("")) {
+                errorPage(request, response, "No ha indicado los grados fahrenheit");
+                return;
+            }
+
+            try {
+                
+                final double value = Double.parseDouble(fahrenheit);
+                final ConversionCF conversion = new ConversionCF(value, "f");
+                resultPage(request, response, conversion.getCelsius(), conversion.getFahrenheit());
+            
+            } catch (NumberFormatException e) {
+                errorPage(request, response, "La temperatura debe ser númerica");
+            }
+        } else {
+            response.sendRedirect("http://localhost:8080/Ejercicio1");
+        }
+
+    }
+
+    private void errorPage(HttpServletRequest request, HttpServletResponse response, String message) {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println(
+                    "<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<title>Error</title>"
+                    + "<link rel='stylesheet' href='http://localhost:8080/Ejercicio1/styles/error.css'>"
+                    + "</head>"
+                    + "<body>"
+                    + "<h2>" + message + "</h1>"
+                    + "<p>Selecciona una temperatura válida</p>"
+                    + "<a id='error' href='http://localhost:8080/Ejercicio1'>Volver</a>"
+                    + "</body>"
+                    + "</html>"
+            );
+        } catch (Exception e) {
+        }
+    }
+
+    private void resultPage(HttpServletRequest request, HttpServletResponse response, double celsius, double fahrenheit) {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            DecimalFormat formatter = new DecimalFormat("#0.00");
+            out.println(
+                    "<!DOCTYPE html>"
+                    + "<html>"
+                    + "<head>"
+                    + "<title>Conversion exitosa</title>"
+                    + "<link rel='stylesheet' href='http://localhost:8080/Ejercicio1/styles/error.css'>"
+                    + "</head>"
+                    + "<body>"
+                    + "<h2>Resultado de la conversion</h1>"
+                    + "<p><b>Valor en Celsius: </b>" + formatter.format(celsius) + "</p>"
+                    + "<p><b>Valor en Fahrenheit: </b>" + formatter.format(fahrenheit) + "</p>"
+                    + "<a href='http://localhost:8080/Ejercicio1'>Volver</a>"
+                    + "</body>"
+                    + "</html>"
+            );
+        } catch (Exception e) {
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
